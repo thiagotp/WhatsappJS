e6034c96c2bc5bbdf2db461a9ca1387a2680bf3f
thiagotp <thiagoteixeira.ps@gmail.com>
2021-04-06 23:43:16 -0300

    Ajustando a caixa de texto da conversa, retirando o placeholder e inserindo emojis no texto

--- src/controller/whatsAppController.js
+++ src/controller/whatsAppController.js
@@ -209,13 +209,60 @@ class WhatsAppController {
             })
         })
 
+        this.el.inputText.on('keypress', e => {
+            if (e.key === 'Enter' && !e.ctrlKey) {
+                e.preventDefault()
+                this.el.btnSend.click()
+            }
+        })
+
+        this.el.inputText.on('keyup', e => {
+            if (this.el.inputText.innerHTML && this.el.inputText.innerHTML != '<br>') {
+                this.el.inputPlaceholder.hide()
+                this.el.btnSendMicrophone.hide()
+                this.el.btnSend.show()
+            } else {
+                this.el.inputPlaceholder.show()
+                this.el.btnSendMicrophone.show()
+                this.el.btnSend.hide()
+            }
+        })
+
+        this.el.btnSend.on('click', e => {
+            console.log(this.el.inputText.innerHTML)
+        })
+
+        this.el.btnEmojis.on('click', e => {
+            this.el.panelEmojis.toggleClass('open')
+        })
+
+        this.el.panelEmojis.querySelectorAll('.emojik').forEach(emoji => {
+            emoji.on('click', e => {
+                console.log(emoji.dataset.unicode)
+                let img = this.el.imgEmojiDefault.cloneNode()
+
+                img.style.cssText = emoji.style.cssText
+                img.dataset.unicode = emoji.dataset.unicode
+                img.alt = emoji.dataset.unicode
+
+                emoji.classList.forEach(name => {
+                    img.classList.add(name)
+                })
+
+                this.el.inputText.appendChild(img)
+                this.el.inputText.dispatchEvent(new Event('keyup'))
+            })
+        })
+
+
+
     }//Método para configurar algumas ações do nosso projeto (ex: ações de botões)
 
     startRecordTime() {
 
         let start = Date.now()
 
-        this._recordMicrophoneInterval = setInterval(()=>{
+        this._recordMicrophoneInterval = setInterval(() => {
             let time = (Date.now() - start)
             this.el.recordMicrophoneTimer.innerHTML = Format.toTime(time)
         }, 100)
