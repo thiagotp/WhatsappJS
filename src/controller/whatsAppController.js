4208deb24934c6debc2dab686843075fa320c05c
thiagotp <thiagoteixeira.ps@gmail.com>
2021-04-07 21:54:40 -0300

    Configurando controller para utilizar a camera do computador/notebook para tirar fotos. (Usando getUserMedia e URL.createObjectURL

--- src/controller/whatsAppController.js
+++ src/controller/whatsAppController.js
@@ -167,6 +167,7 @@ class WhatsAppController {
             this.el.panelCamera.css({
                 height: 'calc(100%)'
             })
+            this._camera = new CameraController(this.el.videoCamera)
         })
 
         this.el.btnClosePanelCamera.on('click', e => {
